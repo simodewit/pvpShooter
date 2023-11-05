dbcae8037ad0df0803405eac3c2f706c1f7ff281
simodewit <117769294+simodewit@users.noreply.github.com>
2023-11-05 02:28:07 +0100

    playercount
    it now tracks player count to see if 2nd player needs to be spawned

--- pvpShooter/Assets/Sripts/Lobby/JoinRoom.cs
+++ pvpShooter/Assets/Sripts/Lobby/JoinRoom.cs
@@ -8,14 +8,31 @@ public class JoinRoom : MonoBehaviourPunCallbacks
     public Transform spawnPlace;
     public PhotonView view;
 
+    int players;
+
     public void Start()
+    {
+        HostJoinCode();
+    }
+
+    public void Update()
+    {
+        if(players != PhotonNetwork.CurrentRoom.PlayerCount)
+        {
+            PhotonNetwork.Instantiate("Player", spawnPlace.position, new Quaternion(0, 0, 0, 0));
+            players = PhotonNetwork.CurrentRoom.PlayerCount;
+        }
+    }
+
+    public void HostJoinCode()
     {
         view = GetComponent<PhotonView>();
 
-        if(!view.IsMine)
+        if (!view.IsMine)
             return;
 
         view.RPC("InstantiatePlayer", RpcTarget.All);
+        players = PhotonNetwork.CurrentRoom.PlayerCount;
     }
 
     [PunRPC]
