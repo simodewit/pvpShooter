10f241366f047cb636385a2a7ec98f1184f6fb33
Atmojomojo <rotastisch@gmail.com>
2023-12-18 14:27:01 +0100

    Players Can Switch team again

--- pvpShooter/Assets/Sripts/Lobby/LobbyScreen/Teams/PlayerTeam.cs
+++ pvpShooter/Assets/Sripts/Lobby/LobbyScreen/Teams/PlayerTeam.cs
@@ -5,9 +5,15 @@ using UnityEngine.UI;
 
 public class PlayerTeam : MonoBehaviour
 {
+    public Button switchTeam;
     public int whatTeam;
-    public void SelectPlayer(Button switchButton)
+
+    public void Awake()
     {
-        switchButton.GetComponent<SwitchTeam>().player = gameObject.GetComponent<Button>();
+        switchTeam = FindAnyObjectByType<SwitchTeam>().GetComponent<Button>();
+    }
+    public void SelectPlayer()
+    {
+        switchTeam.GetComponent<SwitchTeam>().player = gameObject.GetComponent<Button>();
     }
 }
