5644941d80ae53695903bbe7290ab7b5c1319280
Atmojomojo <rotastisch@gmail.com>
2023-12-18 13:49:36 +0100

    On room join spawn PlayerButton in teams

--- pvpShooter/Assets/Sripts/Lobby/JoinRoom.cs
+++ pvpShooter/Assets/Sripts/Lobby/JoinRoom.cs
@@ -3,11 +3,25 @@ using Photon.Pun;
 
 public class JoinRoom : MonoBehaviourPunCallbacks
 {
-    public string playerPrefabName;
+    public GameObject team1, team2;
+    public string playerPrefabName, playerButtonName;
     public Transform spawnPlace;
+    public GameObject spawnedButton;
 
     public void Awake()
     {
         PhotonNetwork.Instantiate(playerPrefabName, spawnPlace.position, new Quaternion(0, 0, 0, 0));
+        if (PhotonNetwork.PlayerList.Length%2 == 0)
+        {
+            spawnedButton = PhotonNetwork.Instantiate(playerButtonName, team1.transform.position, new Quaternion(0, 0, 0, 0));
+            spawnedButton.transform.SetParent(team1.transform, true);
+        }
+        else
+        {
+            spawnedButton = PhotonNetwork.Instantiate(playerButtonName, team2.transform.position, new Quaternion(0, 0, 0, 0));
+            spawnedButton.transform.SetParent(team2.transform, true);
+        }
+        spawnedButton.transform.rotation = new Quaternion(0,0,0,0);
+        spawnedButton.transform.localScale = Vector3.one;
     }
 }
