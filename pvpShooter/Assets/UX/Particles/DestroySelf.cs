f1007e3ddf0385c3c2b1a12408c3c5c1b166f46c
Atmojomojo <mairo@samajo.nl>
2024-01-14 23:10:33 +0100

    Gun and ammo has been placed in the map
    move speed has also been doubled and particles have been altered

--- pvpShooter/Assets/UX/Particles/DestroySelf.cs
+++ pvpShooter/Assets/UX/Particles/DestroySelf.cs
@@ -4,20 +4,11 @@ using UnityEngine;
 
 public class DestroySelf : MonoBehaviour
 {
-    private float timeStamp;
     public float cooldown;
     // Start is called before the first frame update
     void Start()
     {
-        timeStamp = Time.time + cooldown;
+        Destroy(gameObject, cooldown);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (timeStamp < Time.time)
-        {
-            Destroy(gameObject);
-        }
-    }
 }
